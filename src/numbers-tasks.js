6c2d083d4a4e600363281e808c19aa3d93e3d541
olenaweb <olena.nevzorova@gmail.com>
2023-12-26 00:43:45 +0200

    13 pass

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -144,9 +144,7 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-// function getLastDigit( value ) {
-//   throw new Error('Not implemented');
-// }
+
 function getLastDigit(value) {
   let res;
   if (value.toString().length > 1) {
@@ -168,8 +166,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -185,8 +183,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  // D = √(a² + b² + c²)
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -206,8 +205,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const nPow = 10 ** pow;
+  const res = Math.round(num / nPow);
+  return res * nPow;
 }
 
 /**
@@ -227,10 +228,27 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
-}
 
+function isPrime(n) {
+  let result;
+  if (n <= 1) {
+    result = false;
+  } else if (n <= 3) {
+    result = true;
+  } else if (n % 2 === 0 || n % 3 === 0) {
+    result = false;
+  }
+  if (result === undefined) {
+    let i = 5;
+    while (i * i <= n) {
+      if (n % i === 0 || n % (i + 2) === 0) {
+        result = false;
+      }
+      i += 6;
+    }
+  }
+  return result === undefined ? true : result;
+}
 /**
  * Tries to convert value to number and returns it if conversion was successful;
  * otherwise returns default value passed as a second argument.
@@ -246,8 +264,14 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  let result;
+  result = Number(value);
+  if (Number.isNaN(result)) {
+    result = def;
+  }
+  // result = (isNaN(result)) ? def : result;
+  return result;
 }
 
 /**
