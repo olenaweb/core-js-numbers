633223a913f699e3b3fbc5bd70b4624aae21184c
olenaweb <olena.nevzorova@gmail.com>
2024-12-17 01:38:20 +0200

    getCountOfOddNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -701,8 +701,8 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -718,8 +718,9 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const value = number < 0 ? -number : number;
+  return Math.floor((value + 1) / 2);
 }
 
 module.exports = {
