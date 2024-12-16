89e90a47227af82ed665a5380e6ad9e4e562f25c
olenaweb <olena.nevzorova@gmail.com>
2024-12-17 00:54:50 +0200

    getIntegerOnString getFloatOnString

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -545,8 +545,9 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  const value = Number.parseFloat(str);
+  return Number.isNaN(value) ? NaN : value;
 }
 
 /**
@@ -563,8 +564,9 @@ function getFloatOnString(/* str */) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  const value = Number.parseInt(str, base);
+  return Number.isNaN(value) ? NaN : value;
 }
 
 /**
